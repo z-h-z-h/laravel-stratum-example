9c60c280c806c5f5424a1c3bfea27f412fff5abf
Zhdan Zhulanov <zhulanov@gmail.com>
2018-12-05 22:19:57 +0500

    PhpDoc

--- app/Http/Controllers/ElectrumController.php
+++ app/Http/Controllers/ElectrumController.php
@@ -3,18 +3,33 @@
 namespace App\Http\Controllers;
 
 use App\Services\Electrum;
-use BitWasp\Bitcoin\Transaction\TransactionFactory;
 use Illuminate\Http\Request;
 
+/**
+ * Class ElectrumController
+ * @package App\Http\Controllers
+ */
 class ElectrumController extends Controller
 {
+    /**
+     * @var Electrum
+     */
     protected $electrum;
 
+    /**
+     * ElectrumController constructor.
+     * @param Electrum $electrum
+     */
     public function __construct(Electrum $electrum)
     {
         $this->electrum = $electrum;
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector|\Illuminate\View\View
+     * @throws \ErrorException
+     */
     public function getAddressData(Request $request)
     {
         $address = $request->input('address');
@@ -30,6 +45,11 @@ class ElectrumController extends Controller
         return view('address', compact('address','balance', 'history', 'unconfirmed'));
     }
 
+    /**
+     * @param Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector|\Illuminate\View\View
+     * @throws \ErrorException
+     */
     public function getTxData(Request $request)
     {
         $txid = $request->input('txid');
--- app/Services/Electrum.php
+++ app/Services/Electrum.php
@@ -5,13 +5,29 @@ namespace App\Services;
 
 use Datto\JsonRpc\Client;
 
+/**
+ * Class Electrum
+ * @package App\Services
+ */
 class Electrum
 {
 
+    /**
+     * @var string
+     */
     protected $host;
+    /**
+     * @var string
+     */
     protected $port;
+    /**
+     * @var Client
+     */
     protected $client;
 
+    /**
+     * Electrum constructor.
+     */
     public function __construct()
     {
         $this->host = config('electrum.host', 'ssl://electrum.nute.net');
@@ -19,29 +35,52 @@ class Electrum
         $this->client = new Client();
     }
 
+    /**
+     * @param string $address
+     * @return bool|\Datto\JsonRpc\Response[]
+     * @throws \ErrorException
+     */
     public function getAddressBalance(string $address)
     {
         return $this->sendRequest('blockchain.address.get_balance', [$address]);
     }
 
+    /**
+     * @param string $address
+     * @return bool|\Datto\JsonRpc\Response[]
+     * @throws \ErrorException
+     */
     public function getAddressHistory(string $address)
     {
         return $this->sendRequest('blockchain.address.get_history', [$address]);
     }
 
+    /**
+     * @param string $address
+     * @return bool|\Datto\JsonRpc\Response[]
+     * @throws \ErrorException
+     */
     public function getAddressMempool(string $address)
     {
         return $this->sendRequest('blockchain.address.get_mempool', [$address]);
     }
 
+    /**
+     * @param string $txid
+     * @return bool|\Datto\JsonRpc\Response[]
+     * @throws \ErrorException
+     */
     public function getRawTx(string $txid)
     {
         return $this->sendRequest('blockchain.transaction.get', [$txid]);
     }
 
+    /**
+     * @param string $txid
+     * @return string|false
+     */
     public function getTx(string $txid)
     {
-
         $data = rescue(function () use ($txid) {
             return file_get_contents("https://blockchain.info/rawtx/$txid");
         }, function () {
@@ -51,6 +90,12 @@ class Electrum
         return $data;
     }
 
+    /**
+     * @param $method
+     * @param array $params
+     * @return bool|\Datto\JsonRpc\Response[]
+     * @throws \ErrorException
+     */
     public function sendRequest($method, $params = [])
     {
         $json = $this->client->query('myid', $method, $params)->encode();
